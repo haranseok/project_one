1570be9aeb262efe6a41becbb43a2837a50ef15b
haran seok <gkfks444@naver.com>
2023-01-12 18:59:10 +0900

    bar chart 예시

---
+++ src/components/chart/BarChart.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="chart_box">
+        <h2>막대형 차트</h2>
+        <Bar
+        :options="chartOptions"
+        :data="chartData"
+    />
+    </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs';
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+export default {
+    components: { Bar },
+    data() {
+        return {
+            chartData: {
+                labels: [ 'January', 'February', 'March' ],
+                datasets: [ { 
+                    data: [40, 20, 12],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)'
+                    ]} ]            
+            },
+            chartOptions: {
+                responsive: true
+            }
+        }
+    }
+}
+// https://vue-chartjs.org/guide/#chart-with-local-data vue data
+</script>
+
+<style>
+.chart_box{
+    width: 500px;
+}
+</style>
---
+++ src/components/chart/Doughnut.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="chart_box">
+        <h2>도넛 차트</h2>
+        
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
